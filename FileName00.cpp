c96153a19c82466cc0c259b7c6e044df764edd1b
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-30 21:49:27 +0900

    Add files via upload

---
+++ FileName00.cpp
@@ -0,0 +1,48 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <cstring>
+//using namespace std;
+//
+//class Member {
+//private:
+//	char name[20];
+//	char phone[20];
+//	char addr[20];
+//	static int cnt;
+//
+//public:
+//	Member() { }
+//	Member(const char* name, const char* phone, const char* addr) {
+//		strcpy(this->name, name);
+//		strcpy(this->phone, phone);
+//		strcpy(this->addr, addr);
+//	}
+//	static void MemberCount(int);
+//	void insert();
+//	void print();
+//};
+//
+//
+//int Member::cnt = 0;
+//
+//void Member::insert() {
+//	cout << "이름, 전화번호, 주소 입력 : ";
+//	cin >> name >> phone >> addr;
+//	cnt++;
+//}
+//
+//void Member::print() {
+//	cout << "이름 : " << name << "전화번호 : " << phone << "주소 : " << addr << "객체 수 : " << cnt << endl;
+//}
+//
+//
+//int main() {
+//	Member m;
+//
+//	for (int i = 0; i < 3; i++) {
+//		m.insert();
+//		m.print();
+//	}
+//
+//	return 0;
+//}
